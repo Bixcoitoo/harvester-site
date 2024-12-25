d5094a179bedc9e946fc442ecf8915e7fb7e8e95
Bixcoitoo <isacmagalha123@gmail.com>
2024-12-24 23:54:08 -0300

    dd

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,17 +1,6 @@
-const configs = {
-    development: {
-        API_URL: 'http://localhost:8080/api/v1',
-        RECAPTCHA_SITE_KEY: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
-    },
-    staging: {
-        API_URL: 'https://harvester-api.herokuapp.com/api/v1',
-        RECAPTCHA_SITE_KEY: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
-    },
-    production: {
-        API_URL: 'https://api.harvesterdownloader.site/api/v1',
-        RECAPTCHA_SITE_KEY: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
-    }
-};
-
-const ENV = process.env.NODE_ENV || 'development';
-export const { API_URL, RECAPTCHA_SITE_KEY } = configs[ENV]; 
+const config = {
+    RECAPTCHA_SITE_KEY: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', // Chave de teste
+    API_URL: window.location.hostname === 'localhost' 
+        ? 'http://localhost:5001'
+        : 'https://harvester-api-42f53e6844e5.herokuapp.com'
+}; 
